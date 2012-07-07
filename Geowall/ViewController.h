703a45ee9d1d44e9dc544efc467dd390c7ca6c02
Vittorio Cuculo <lasek@MacBook-Pro-di-Giulia.local>
2012-07-07 14:48:47 +0200

    Initial Commit

---
+++ Geowall/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Geowall
+//
+//  Created by Vittorio Cuculo on 07/07/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
